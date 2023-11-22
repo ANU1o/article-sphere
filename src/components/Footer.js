c287f31d3945fd74ba21f30ffc5192bdf7589d24
ANU 10 <78339400+ANU1o@users.noreply.github.com>
2023-11-22 07:05:23 +0300

    footerurl

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,28 +29,34 @@ const Footer = ({ displayVal }) => {
         <hr className="border-primary opacity-50 border-3 my-3 d-block d-lg-none" />
         <Col lg xs="12">
           <h1 className="text-primary">Libraries</h1>
-          <Link to="/Home" className="nav-link">
+          <Link to="https://reactrouter.com/en/main" className="nav-link">
             react-router-dom
           </Link>
-          <Link to="/Home" className="nav-link">
+          <Link to="https://react-bootstrap.netlify.app" className="nav-link">
             react-bootstrap
           </Link>
-          <Link to="/Home" className="nav-link">
+          <Link to="https://firebase.google.com" className="nav-link">
             Firebase
           </Link>
         </Col>
         <hr className="border-primary opacity-50 border-3 my-3 d-block d-lg-none" />
         <Col className="d-flex flex-row justify-content-center">
-          <Link to="/Home" className="nav-link fs-3">
+          <Link to="https://github.com/ANU1o" className="nav-link fs-3">
             <i className="fa-brands fa-github"></i>
           </Link>
-          <Link to="/Home" className="nav-link fs-3">
+          <Link
+            to="https://www.linkedin.com/in/anoof-abdul-kadar-883433147/"
+            className="nav-link fs-3"
+          >
             <i className="fa-brands fa-linkedin"></i>
           </Link>
-          <Link to="/Home" className="nav-link fs-3">
+          <Link to="https://www.behance.net/anu1o" className="nav-link fs-3">
             <i className="fa-brands fa-behance"></i>
           </Link>
-          <Link to="/Home" className="nav-link fs-3">
+          <Link
+            to="https://www.instagram.com/anu_1o_/"
+            className="nav-link fs-3"
+          >
             <i className="fa-brands fa-instagram"></i>
           </Link>
         </Col>
